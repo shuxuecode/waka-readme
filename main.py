fca1adb7b669334df0b30dd7e83ac52efe3a4a19
shuxuecode <zhaoshuxue@163.com>
2021-09-10 00:24:25 +0800

    4

--- main.py
+++ main.py
@@ -55,8 +55,9 @@ def get_stats() -> str:
         }).json()
     try:        
         lang_data = data['data']['human_readable_total']
-        if 1==1:
-            return '```text\n'+this_week()+'\n\n'+lang_data+'\n```'
+        if 1==1:            
+            # return '```text\n'+this_week()+'\n\n'+lang_data+'\n```'
+            return '```text\nLast 7 days coding time :\n\n'+lang_data+'\n```'
     except KeyError:
         print("Please Add your WakaTime API Key to the Repository Secrets")
         sys.exit(1)
